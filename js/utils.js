9fa440b7b29b943c71616793ca08ad67ee54e094
kawsar firoz <kawsarfiroz11@gmail.com>
2024-09-22 20:22:09 +0300

    added all design navbar1, navbar2, blogsection, modal

---
+++ js/utils.js
@@ -0,0 +1,17 @@
+
+
+
+const getElementById = (id)=>{
+    return document.getElementById(id);
+}
+
+
+// get elemen by class name 
+const getElementByClassName = (name)=>{
+    return document.getElementsByClassName(name);
+}
+
+// toogle class 
+const toogleClass =(className) =>{
+
+}
