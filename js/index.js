0dedcbe5005933d7f92367d610d6d75742f1196f
kawsar firoz <kawsarfiroz11@gmail.com>
2024-09-23 12:35:09 +0300

    updated on js file , showing ammount,

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@ const showblog = document.getElementById("showblog");
 const showHistory = document.getElementById("showHistory");
 const donat_container = document.getElementById("donat_container");
 const history_container = document.getElementById("history_container");
-
+const closeModel = getElementById("closeModel");
+const modal = getElementById("modal");
 showblog.addEventListener('click', (e) => {
     e.preventDefault();
     history_container.classList.add("hidden");       
@@ -21,62 +22,77 @@ showHistory.addEventListener("click", (e) => {
     history_container.classList.remove("hidden");    
     donat_container.classList.add("hidden");         
     showHistory.classList.add("bg-btn_bg");          
-    showblog.classList.remove("bg-btn_bg")
-
+    showblog.classList.remove("bg-btn_bg");
 });
 
+closeModel.addEventListener('click',(e)=>{
+    modal.classList.add("hidden")
+    modal.classList.remove("flex")
+})
 
 
 
-// global variable
+
+
+
+// Global variable
 let sum = 5500;
 const mainAmount = getElementById('mainAmount');
 
 
-const handleDonation =(inputVal,val,tittle)=>{    
+
+const handleDonation = (inputVal, val, title) => {
+    let value= parseInt(val.innerHTML);
+ 
+   
+   
     let date = new Date();
-    if (isNaN(inputVal) || inputVal <= 0  || sum <=0) {
-        alert("invalid input ");
+    if (isNaN(inputVal) || inputVal <= 0 || sum <= 0 || inputVal === "") {
+        alert("Invalid Donate ammount");
     } else {
-        sum -=inputVal;
-        mainAmount.innerText =  sum;
-        history_container.innerHTML+=`
-        <div class="flex  flex-col justify-start items-start gap-3 border p-5 rounded w-[100%]">
-                <h1 class="text-2xl font-semibold capitalize"> ${inputVal} ${tittle.innerText}</h1>
-                <p class="text-[gray] capitalize">${date}</p>
-            </div>
-        
-        `
-       
+        sum -= inputVal;
+        val.innerHTML = value += inputVal ;
+        mainAmount.innerText = sum;
+        modal.classList.add("flex")
+        modal.classList.remove("hidden");
+        history_container.innerHTML +=`
+        <div class="flex flex-col justify-start items-start gap-3 border p-5 rounded w-[100%]">
+            <h1 class="text-2xl font-semibold capitalize">${inputVal} ${title.innerText}</h1>
+            <p class="text-[gray] capitalize">${date}</p>
+        </div>
+        `;
     }
-}
+};
 
-const handleClickOne = (e)=>{
-    let input_ammount_1 = getElementById("input_ammount_1");
-    console.log(input_ammount_1.innerHTML)
-    let title_1 = getElementById("title_1");
-    const inputfiled_1 = parseInt(getElementById("inputfiled_1").value);
-    let input_ammount_1a_= parseInt(input_ammount_1.innerText)
-    input_ammount_1a_ += inputfiled_1;    
-    handleDonation(inputfiled_1, input_ammount_1, title_1)
-}
-// 
-const handleClickTwo = (e)=>{
-    let input_ammount_2 = getElementById("input_ammount_2");
-    let title_2 = getElementById("title_2");
-    const inputfiled_2 = parseInt(getElementById("inputfiled_2").value);
-    input_ammount_2.innerText += inputfiled_2
-    handleDonation(inputfiled_2, input_ammount_2, title_2)
-}
+// Function for first donation click event
+const handleClickOne = (e) => {
+    let inputAmount1 = getElementById("input_ammount_1");
+    let title1 = getElementById("title_1");
+    const inputField1 = parseInt(getElementById("inputfiled_1").value);
+    // let inputAmount1Value = parseInt(inputAmount1.innerHTML);
+     
+    handleDonation(inputField1, inputAmount1, title1);
+};
 
-// 
-const handleClickthree = (e)=>{
-    let input_ammount_3 = getElementById("input_ammount_3");
-    let title_3 = getElementById("title_3");
-    const inputfiled_3 = parseInt(getElementById("inputfiled_3").value);
-    input_ammount_3.innerText += inputfiled_3
-    handleDonation(inputfiled_3, input_ammount_3, title_3)
+// Function for second donation click event
+const handleClickTwo = (e) => {
+    let inputAmount2 = getElementById("input_ammount_2");
+    let title2 = getElementById("title_2");
+    const inputField2 = parseInt(getElementById("inputfiled_2").value);
+    // const inputAmount1Value = parseInt(inputAmount2.innerText);
+    // inputAmount2.innerText += inputField2;
+    handleDonation(inputField2, inputAmount2, title2);
+};
 
-}
+// Function for third donation click event
+const handleClickThree = (e) => {
+    let inputAmount3 = getElementById("input_ammount_3");
+    let title3 = getElementById("title_3");
+    const inputField3 = parseInt(getElementById("inputfiled_3").value);
+    // let inputAmount1Value = parseInt(inputAmount3.innerText);
+
+    // inputAmount1Value += inputField3;
+    handleDonation(inputField3, inputAmount3, title3);
+};
 
 
